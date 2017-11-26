0c9a2ca57fe89b6cff999739a5242848e0115c67
Chris Butler <chris@platformthirteen.com>
2017-11-26 16:55:59 -0500

    refactor and cleanup  - remove cruft  - add TimeTimer  - format countdown correctly  - fix markup generation

--- MMM-PomoDone.js
+++ MMM-PomoDone.js
@@ -4,79 +4,180 @@
 Module.register('MMM-PomoDone', {
   defaults: {
     url: 'https://pomodone-magicmirror.glitch.me/timer/status',
-    pollFrequency: 1000 * 1,
+    pollFrequency: 1000 * 10
   },
-  getStyles: function () {
-    return ["MMM-PomoDone.css"];
+  getStyles: function() {
+    return ['MMM-PomoDone.css', 'MMM-TimeTimer.css'];
+  },
+  getScripts: function() {
+    return ["moment.js", this.file("node_modules/moment-duration-format/lib/moment-duration-format.js"), this.file("node_modules/countdown/countdown.js"), this.file("node_modules/moment-countdown/dist/moment-countdown.min.js")];
   },
   start: function() {
     Log.info(`Starting ${this.name}`);
 
-    this.status = {};
     this.loading = false;
     this.isHidden = false;
 
-    setInterval(() => {
-      this.getData();
-    }, this.config.pollFrequency);
+    this.response = {};
+    this.timer = () => ({});
+
+    this.ticker = '';
+    this.task = '';
+
+    this.getData();
+
+    console.log('module start', this);
   },
   getData: function() {
-    console.log('getData', this);
+    console.log('getData', this.response);
     if (this.config.url) {
-      this.sendSocketNotification("POMODONE_STATUS_GET", {url: this.config.url, instanceId: this.identifier});
+      this.sendSocketNotification("POMODONE_STATUS_GET", {
+        url: this.config.url,
+        instanceId: this.identifier
+      });
     } else {
       this.hide(1000, {lockString: this.identifier});
       this.isHidden = true;
     }
   },
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === 'POMODONE_STATUS_RESPONSE' + this.identifier) {
+      console.log('*** MMM-PomoDone responseReceived', payload);
+      this.response = payload;
+
+      if (this.response.task) {
+        this.task = this.response.task;
+      }
+
+      if (this.response.minutes) {
+        const end = moment(this.response.start).add(this.response.minutes, 'minutes');
+        this.timer = () => {
+          return moment.duration(end.diff(moment()));
+        }
+
+        console.log('building timer', this.ticker, this.response);
+
+        if (this.loading) {
+          this.loading = false;
+
+          if (this.isHidden) {
+            this.updateDom();
+            this.show(1000, {lockString: this.identifier});
+          } else {
+            this.updateDom(1000);
+          }
+
+        } else {
+          this.updateDom();
+          this.show(1000, {lockString: this.identifier});
+        }
+
+        this.isHidden = false;
+      } else if (this.response.stop) {
+        this.timer = () => ({});
+        this.hide(0, {lockString: this.identifier});
+        this.isHidden = true;
+      }
+    }
+  },
   getDom: function() {
-    console.log('getDom', this.status);
+    console.log('getDom');
     var wrapper = document.createElement("div");
 
     if (this.loading) {
       var loading = document.createElement("div");
-        loading.innerHTML = this.translate("LOADING");
-        loading.className = "dimmed light small";
-        wrapper.appendChild(loading);
-      return wrapper
+
+      loading.innerHTML = this.translate("LOADING");
+      loading.className = "bright light small";
+
+      wrapper.appendChild(loading);
     }
 
-    var row = document.createElement("div");
-    row.classList.add("row");
+    if (this.response.stop) {
+      console.log('timer stopped');
+      clearInterval(this.ticker);
+      this.ticker = '';
 
-    var heading = document.createElement("span");
-    heading.className = "destination-label bright";
-    heading.innerHTML = `${this.status.minutes}:${this.status.seconds}`;
+      var loading = document.createElement("div");
 
-    var summary = document.createElement("div");
-    summary.classList.add("route-summary");
-    summary.innerHTML = 'testing test';
+      loading.innerHTML = this.translate("FINISHED!");
+      loading.className = "bright light small";
 
-    row.appendChild(heading);
-    row.appendChild(summary);
-    wrapper.appendChild(row);
+      wrapper.appendChild(loading);
+    }
+
+    if (this.timer().minutes) {
+      if (!this.ticker) {
+        this.ticker = setInterval(() => {
+          if (!this.timer) {
+            clearInterval(this.ticker);
+            this.ticker = '';
+          }
+          this.updateDom();
+        }, 1000);
+      }
+
+      var row = document.createElement("div");
+      row.classList.add("MMM-PomoDone");
+
+      var heading = document.createElement("div");
+      heading.className = "task-name dimmed";
+      heading.innerHTML = this.task;
+
+      var summary = document.createElement("div");
+      summary.classList.add("timer");
+
+      const timeRemaining = moment.duration({
+        minutes: this.timer()['minutes']() + 1,
+        seconds: this.timer()['seconds']()
+      });
+
+      const secondsRemaining = timeRemaining.asSeconds();
+
+      var timeTimer = document.createElement("div");
+      timeTimer.classList.add("time-timer");
+
+      var ticks = document.createElement('span');
+      ticks.classList.add('ticks');
+
+      timeTimer.innerHTML = `
+        <svg style="stroke-dasharray: ${ 360 - (secondsRemaining % 360)}% 360%"
+          class="ticker" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+            <path d="m50 1.5c-26.5 0-48 22-48 48.5 0 26.5 21.5 48 48 48.5 26.5 0 48-22 48.5-48.5-.5-26.5-21.5-48.5-48-48.5h-.5" fill="none" stroke="#eee"></path>
+        </svg>`;
+
+      for (var i = 1; i <= 60; i++) {
+        let tick = document.createElement('span');
+        tick.classList.add(`tick-${i}`);
+        ticks.appendChild(tick);
+      }
+
+      timeTimer.appendChild(ticks);
+
+      console.log('clock', timeTimer);
+
+      row.appendChild(timeTimer);
+
+      if (this.timer().minutes)
+        console.log('getDom timer', this.timer().minutes(), this.timer().seconds(), secondsRemaining);
+
+      summary.innerHTML = timeRemaining.format('m:', {trim: 'left'});
+
+      var seconds = document.createElement('span');
+      seconds.classList.add('seconds')
+      seconds.innerHTML = timeRemaining.format('m:ss').replace(/^(.*):/, '');
+
+      summary.appendChild(seconds);
+
+      row.appendChild(heading);
+      row.appendChild(summary);
+
+      wrapper.appendChild(row);
+    }
 
     return wrapper;
   },
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === 'POMODONE_STATUS_RESPONSE' + this.identifier) {
-      console.log('responseReceived', payload);
-      this.status = payload;
-      if (this.loading) {
-        this.loading = false;
-        if (this.isHidden) {
-          this.updateDom();
-          this.show(1000, {lockString: this.identifier});
-        } else {
-          this.updateDom(1000);
-        }
-      } else {
-        this.updateDom();
-        this.show(1000, {lockString: this.identifier});
-      }
-      this.isHidden = false;
-    }
-  },
+
   notificationReceived: function(notification, payload, sender) {
     if (notification == 'DOM_OBJECTS_CREATED') {
       this.hide(0, {lockString: this.identifier});
--- node_helper.js
+++ node_helper.js
@@ -1,40 +1,100 @@
 var NodeHelper = require("node_helper");
 var request = require('request');
 var moment = require('moment');
+var WebSocket = require('ws');
+
+function WebSocketClient() {
+  this.number = 0; // Message number
+  this.autoReconnectInterval = 5 * 1000; // ms
+}
+WebSocketClient.prototype.open = function(url) {
+  this.url = url;
+  this.instance = new WebSocket(this.url);
+  this.instance.on('open', () => {
+    this.onopen();
+  });
+  this.instance.on('message', (data, flags) => {
+    this.number++;
+    this.onmessage(data, flags, this.number);
+  });
+  this.instance.on('close', (e) => {
+    switch (e) {
+      case 1000: // CLOSE_NORMAL
+        console.log("WebSocket: closed");
+        break;
+      default: // Abnormal closure
+        this.reconnect(e);
+        break;
+    }
+    this.onclose(e);
+  });
+  this.instance.on('error', (e) => {
+    switch (e.code) {
+      case 'ECONNREFUSED':
+        this.reconnect(e);
+        break;
+      default:
+        this.onerror(e);
+        break;
+    }
+  });
+}
+WebSocketClient.prototype.send = function(data, option) {
+  try {
+    this.instance.send(data, option);
+  } catch (e) {
+    this.instance.emit('error', e);
+  }
+}
+WebSocketClient.prototype.reconnect = function(e) {
+  console.log(`WebSocketClient: retry in ${this.autoReconnectInterval}ms`, e);
+  this.instance.removeAllListeners();
+  var that = this;
+  setTimeout(function() {
+    console.log("WebSocketClient: reconnecting...");
+    that.open(that.url);
+  }, this.autoReconnectInterval);
+}
+WebSocketClient.prototype.onopen = function(e) {
+  console.log("WebSocketClient: open", arguments);
+}
+WebSocketClient.prototype.onmessage = function(data, flags, number) {
+  console.log("WebSocketClient: message", arguments);
+}
+WebSocketClient.prototype.onerror = function(e) {
+  console.log("WebSocketClient: error", arguments);
+}
+WebSocketClient.prototype.onclose = function(e) {
+  console.log("WebSocketClient: closed", arguments);
+}
 
 module.exports = NodeHelper.create({
   start: function() {
     console.log("@xxxxxx[{::::::::::::::::::::::::::::::::::> Starting node_helper for [" + this.name + "]");
   },
-  socketNotificationReceived: function(notification, payload){
+  socketNotificationReceived: function(notification, payload) {
     if (notification === 'POMODONE_STATUS_GET') {
       this.getTimeStatus(payload);
     }
   },
-	getTimeStatus: function(payload) {
-		var self = this;
+  getTimeStatus: function(payload) {
+    var that = this;
     var returned = 0;
     var status = {};
 
-		request({ url: payload.url, method: 'GET' }, function(error, response, body) {
-      if(!error && response.statusCode == 200){
-        var data = JSON.parse(body);
+    var wsc = new WebSocketClient();
+    wsc.open('ws://pomodone-magicmirror.glitch.me/timer/status');
 
-        if (data.error) {
-          console.log("MMM-PomoDone error: ", data.error);
-          status.error = true;
-        } else {
-          console.log("MMM-PomoDone status data: ", data);
-          status = data;
-        }
-      } else {
-        console.log( "Error getting PomoDone timer status: ", response);
-        status.error = true;
-      }
+		wsc.onopen = function(e) {
+      console.log('WebSocketClient connected:', e);
+      this.send('connected');
+    }
 
-      if (!status.error) {
-        self.sendSocketNotification('POMODONE_STATUS_RESPONSE' + payload.instanceId, status);
-      };
-    });
-	}
+		wsc.onmessage = function(msg, flags, number) {
+      console.log(`WebSocketClient message #${number}: `, msg, flags);
+      var data = JSON.parse(msg);
+      that.sendSocketNotification('POMODONE_STATUS_RESPONSE' + payload.instanceId, data);
+      // socket.send(`data received by MagicMirror ${msg}`);
+    }
+  }
 });
